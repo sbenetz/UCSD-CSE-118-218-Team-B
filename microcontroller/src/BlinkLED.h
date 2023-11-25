4a43bb766304949fbacbf907709c8edc1f2ab1c3
sbenetz <sbenetz@ucsd.edu>
2023-11-25 13:16:50 -0800

    setup http and wifi

--- microcontroller/src/BlinkLED.h
+++ microcontroller/src/BlinkLED.h
@@ -2,34 +2,37 @@
 #include <limits>
 class OnboardLED {
     public:
-    bool ON;
-    uint8_t count_;
-    uint8_t max_;
-    uint32_t period_;
-    long next_toggle_;
-    void begin(){
-        ON = false;
-        pinMode(5,OUTPUT);
-        digitalWrite(5,LOW);
-    }
-    void toggleLED() {
-        digitalWrite(5, !ON); // write inversed state back
-        ON = !ON;
-    }
-    void doBlink(){
-        if (count_< max_ || max_ == std::numeric_limits<uint8_t>::max()){
-            if (millis() > next_toggle_){
-                toggleLED();
-                next_toggle_ += period_;
-                count_++;
-            }
+        bool ON;
+        uint8_t count_;
+        uint8_t max_;
+        uint32_t period_;
+        long next_toggle_;
+        void begin(){
+            ON = false;
+            pinMode(BUILTIN_LED,OUTPUT);
+            digitalWrite(BUILTIN_LED,LOW);
         }
-    }
-    // default blink once per second infinitely
-    void setBlinkOnboardLED(uint8_t times = std::numeric_limits<uint8_t>::max(), uint32_t period = 500 /*ms*/){
-        next_toggle_ = millis() + period;
-        period_ = period;
-        max_ = times * 2;
-        count_ = 0;
-    }
+        void toggleLED() {
+            digitalWrite(BUILTIN_LED, !ON); // write inversed state back
+            ON = !ON;
+        }
+        void doBlink(){
+            if (count_< max_){
+                if (millis() > next_toggle_){
+                    toggleLED();
+                    next_toggle_ += period_;
+                    if (max_ != std::numeric_limits<uint8_t>::max()) count_++;
+                    return;
+                }
+            }
+            digitalWrite(BUILTIN_LED,LOW);
+        }
+        // default blink once per second infinitely
+        void setBlinkOnboardLED(uint8_t times = std::numeric_limits<uint8_t>::max(), uint32_t period = 500 /*ms*/){
+            next_toggle_ = millis() + period;
+            period_ = period;
+            if (times < std::numeric_limits<uint8_t>::max()) max_ = times * 2;
+            else max_ = times;
+            count_ = 0;
+        }
 };
--- microcontroller/src/src.ino
+++ microcontroller/src/src.ino
@@ -1,11 +1,19 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 #include <Wire.h>
 #include <BH1750.h>
 #include "BlinkLED.h"
+#include "Credentials.h"
 
 BH1750 light_meter; 
 OnboardLED onBoardLED;
-bool onboard_LED_on = true;
+HTTPClient http;
+
+#ifndef  ARRAY_SIZE
+#define  ARRAY_SIZE 10
+#endif
+uint32_t last_soil_readings [ARRAY_SIZE];
+uint32_t last_sunlight_readings [ARRAY_SIZE];
 void setup() {
     // start up objects
     Serial.begin(115200);
@@ -14,16 +22,17 @@ void setup() {
     light_meter.begin();
     // startup onboard LED code
     onBoardLED.begin();
-    onBoardLED.setBlinkOnboardLED();
+    //onBoardLED.setBlinkOnboardLED();
     // wifi initialization
-    WiFi.begin("StellaRosaGuest", "gentlecarrot408");
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    Serial.print("Connecting to WiFi...");
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
-        Serial.println("Connecting to WiFi...");
+        Serial.print(".");
     }
+    Serial.println("");
     Serial.println("Connected to WiFi");
     // setup pins
-    pinMode(5,OUTPUT); // onboard LED
     pinMode(25,OUTPUT); // powers soil moisture sensor
     pinMode(26,OUTPUT); // grounds soil moisture sensor
     pinMode(33,ANALOG); // reads value from soil moisture sensor
@@ -33,13 +42,46 @@ void setup() {
 }
 
 void loop() {
+    static uint8_t i = 0;
+    // if(WiFi.status()== WL_CONNECTED){
+    //     HTTPClient http;
+    //     http.begin("https://api.weather.gov/points/39.7456,-97.0892");
+    //     int httpResponseCode = http.GET();
+    //     if (httpResponseCode>0) {
+    //         Serial.print("HTTP Response code: ");
+    //         Serial.println(httpResponseCode);
+    //         String payload = http.getString();
+    //         Serial.println(payload);
+    //     }
+    //     else {
+    //         Serial.print("Error code: ");
+    //         Serial.println(httpResponseCode);
+    //     }
+    //     // Free resources
+    //     http.end();
+    // }
+    if (i < ARRAY_SIZE){
+        last_soil_readings[i] = analogReadMilliVolts(33);
+        last_sunlight_readings[i] = (uint32_t)light_meter.readLightLevel();
+        i++;
+    }
+    else {
+        uint32_t soil_average = getAverage(last_soil_readings);
+        uint32_t light_average = getAverage(last_sunlight_readings);
+        Serial.printf("Soil_Moisture_(mV): %d, ",soil_average);
+        Serial.printf("Light_(lx): %d \n",light_average);
+        i = 0;
+        onBoardLED.setBlinkOnboardLED(3);
+    }
     
-    String mv = String(analogReadMilliVolts(33));
-    Serial.print("Soil_Moisture_(mV):"+mv +", " );
-    float lux = light_meter.readLightLevel();
-    Serial.print("Light_(lx):");
-    Serial.print(lux);
-    Serial.println();
+
     onBoardLED.doBlink(); // run blinks if setup
-    delay(1000); // Delay for a second, adjust as needed
+    delay(100);
+}
+
+uint32_t getAverage(uint32_t array[ARRAY_SIZE]){
+    uint64_t sum = 0;
+    for(uint8_t j=0; j<ARRAY_SIZE;j++)
+        sum += array[j];
+    return (uint32_t)(sum/ARRAY_SIZE);
 }
