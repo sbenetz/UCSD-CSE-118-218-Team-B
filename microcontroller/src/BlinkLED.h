94cf1ac4a13e6fedf1a6b6d1e9f62c6b7c7c5c19
sbenetz <sbenetz@ucsd.edu>
2023-11-28 12:07:32 -0800

    attempt 1

--- microcontroller/src/BlinkLED.h
+++ microcontroller/src/BlinkLED.h
@@ -4,38 +4,48 @@
 #ifndef BUILTIN_LED
 #define BUILTIN_LED 5
 #endif
-class OnboardLED {
-    public:
-        bool ON;
-        uint8_t count_;
-        uint8_t max_;
-        uint32_t period_;
-        long next_toggle_;
-        void begin(){
-            ON = false;
-            pinMode(BUILTIN_LED,OUTPUT);
-            digitalWrite(BUILTIN_LED,!ON);
-        }
-        // HIGH = OFF, LOW = ON
-        void toggleLED() {
-            digitalWrite(BUILTIN_LED, !ON); // write inversed state back
-            ON = !ON;
-        }
-        void doBlink(){
-            if (count_< max_){
-                if (millis() > next_toggle_){
-                    toggleLED();
-                    next_toggle_ += period_;
-                    if (ON && max_ != std::numeric_limits<uint8_t>::max() ) count_++;
-                    return;
-                }
+class OnboardLED
+{
+public:
+    bool ON;
+    uint8_t count_;
+    uint8_t max_;
+    uint32_t period_;
+    long next_toggle_;
+    void begin()
+    {
+        ON = false;
+        pinMode(BUILTIN_LED, OUTPUT);
+        digitalWrite(BUILTIN_LED, !ON);
+    }
+    // HIGH = OFF, LOW = ON
+    void toggleLED()
+    {
+        digitalWrite(BUILTIN_LED, !ON); // write inversed state back
+        ON = !ON;
+    }
+    void doBlink()
+    {
+        if (count_ < max_)
+        {
+            if (millis() > next_toggle_)
+            {
+                Serial.printf("Millis: %f, Next: %f\n", millis(), next_toggle_);
+                toggleLED();
+                next_toggle_ += (long)period_;
+                if (!ON && max_ != std::numeric_limits<uint8_t>::max())
+                    count_++;
             }
+            Serial.printf("Count: %d, Max: %d, Next: %f\n", count_, max_, next_toggle_);
+            return;
         }
-        // default blink once per second infinitely
-        void setBlinkOnboardLED(uint8_t times = std::numeric_limits<uint8_t>::max(), uint32_t period = 500 /*ms*/){
-            next_toggle_ = millis() + period;
-            period_ = period;
-            max_ = times;
-            count_ = 0;
-        }
+    }
+    // default blink once per second infinitely
+    void setBlinkOnboardLED(uint8_t times = std::numeric_limits<uint8_t>::max(), uint32_t period = 500 /*ms*/)
+    {
+        next_toggle_ = millis() + period;
+        period_ = period;
+        max_ = times;
+        count_ = 0;
+    }
 };
--- microcontroller/src/src.ino
+++ microcontroller/src/src.ino
@@ -28,66 +28,66 @@ HTTPClient http;
 
 void waterPumpOn(uint time_ms)
 {
-    digitalWrite(WATER_PUMP_VCC, HIGH);
-    digitalWrite(WATER_PUMP_SIG, HIGH);
-    delay(time_ms);
-    digitalWrite(WATER_PUMP_VCC, LOW);
-    digitalWrite(WATER_PUMP_SIG, LOW);
+  digitalWrite(WATER_PUMP_VCC, HIGH);
+  digitalWrite(WATER_PUMP_SIG, HIGH);
+  delay(time_ms);
+  digitalWrite(WATER_PUMP_VCC, LOW);
+  digitalWrite(WATER_PUMP_SIG, LOW);
 }
 void IOBegin()
 {
-    // BH1750 light meter
-    Wire.begin();
-    // pins are already set up through SCL & SDA
-    lightMeter.begin();
-    // startup onboard LED code
-    builtinLED.begin();
-    // begin Bluetooth server and wifi if available
-    beginRFServices();
-    // setup pins
-    pinMode(25, OUTPUT); // powers soil moisture sensor
-    pinMode(26, OUTPUT); // grounds soil moisture sensor
-    pinMode(33, ANALOG); // reads value from soil moisture sensor
-    // initalize pin values
-    digitalWrite(25, HIGH);
-    digitalWrite(26, LOW);
+  // BH1750 light meter
+  Wire.begin();
+  // pins are already set up through SCL & SDA
+  lightMeter.begin();
+  // startup onboard LED code
+  builtinLED.begin();
+  // begin Bluetooth server and wifi if available
+  beginRFServices();
+  // setup pins
+  pinMode(25, OUTPUT); // powers soil moisture sensor
+  pinMode(26, OUTPUT); // grounds soil moisture sensor
+  pinMode(33, ANALOG); // reads value from soil moisture sensor
+  // initalize pin values
+  digitalWrite(25, HIGH);
+  digitalWrite(26, LOW);
 }
 
 void loop()
 {
-    static uint8_t i = 0;
-    inLoop();
-    if (i < ARRAY_SIZE)
+  static uint8_t i = 0;
+  inLoop();
+  if (i < ARRAY_SIZE)
+  {
+    soilReadingHistory[i] = (uint8_t)((double)(soil_moisture_max - analogReadMilliVolts(33)) / (double)(soil_moisture_max - soil_moisture_min) * 100.0);
+    sunlightReadingHistory[i] = (uint32_t)lightMeter.readLightLevel();
+    i++;
+  }
+  else
+  {
+    uint32_t soil_average = getAverage(soilReadingHistory);
+    uint32_t light_average = getAverage(sunlightReadingHistory);
+    Serial.printf("Soil_Moisture (%): %d, ", soil_average);
+    Serial.printf("Soil Moisture (mV): %d, ", analogReadMilliVolts(SOIL_SENSOR_SIG));
+    Serial.printf("Light_(lx): %d \n", light_average);
+    builtinLED.setBlinkOnboardLED(3);
+    watering_level = postSensorReadings(deviceID, soil_average, light_average);
+    if ((int)soil_average < watering_level)
     {
-        soilReadingHistory[i] = (uint8_t)((double)(soil_moisture_max - analogReadMilliVolts(33)) / (double)(soil_moisture_max - soil_moisture_min) * 100.0);
-        sunlightReadingHistory[i] = (uint32_t)lightMeter.readLightLevel();
-        i++;
+      Serial.println("Watering");
+      waterPumpOn(3000);
     }
-    else
-    {
-        uint32_t soil_average = getAverage(soilReadingHistory);
-        uint32_t light_average = getAverage(sunlightReadingHistory);
-        Serial.printf("Soil_Moisture (%): %d, ", soil_average);
-        Serial.printf("Soil Moisture (mV): %d, ", analogReadMilliVolts(SOIL_SENSOR_SIG));
-        Serial.printf("Light_(lx): %d \n", light_average);
-        builtinLED.setBlinkOnboardLED(3);
-        watering_level = postSensorReadings(deviceID, soil_average, light_average);
-        if ((int)soil_average < watering_level)
-        {
-            Serial.println("Watering");
-            waterPumpOn(3000);
-        }
-        i = 0;
-    }
+    i = 0;
+  }
 
-    builtinLED.doBlink(); // run blinks if setup
-    delay(500);
+  builtinLED.doBlink(); // run blinks if setup
+  delay(500);
 }
 
 uint32_t getAverage(uint32_t array[ARRAY_SIZE])
 {
-    uint64_t sum = 0;
-    for (uint8_t j = 0; j < ARRAY_SIZE; j++)
-        sum += array[j];
-    return (uint32_t)(sum / ARRAY_SIZE);
+  uint64_t sum = 0;
+  for (uint8_t j = 0; j < ARRAY_SIZE; j++)
+    sum += array[j];
+  return (uint32_t)(sum / ARRAY_SIZE);
 }
