484155f8c26c694c42e1625049b671e1e7309518
Jackson-Wheeler <jackson.n.wheeler@gmail.com>
2023-12-05 15:13:58 -0800

    updated build script #38

--- server/build.sh
+++ server/build.sh
@@ -7,11 +7,19 @@ UPDATE_SCRIPT_PATH=server_scripts/${UPDATE_SCRIPT_NAME}
 RUN_SCRIPT_NAME=runServer.sh
 RUN_SCRIPT_PATH=server_scripts/${RUN_SCRIPT_NAME}
 USERNAME=root
-HOSTNAME="8.tcp.us-cal-1.ngrok.io"
-HOST_PORT=12976
 
 API_SERVER_DIR=server
 API_SERVER_PORT=8000
+
+# Check if both host and port arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <host> <port>"
+    exit 1
+fi
+
+# Assign command-line arguments to variables
+HOST="$1"
+PORT="$2"
 
 echo "Building to Raspberry Pi"
 
@@ -20,14 +28,14 @@ echo "cmd: zip -r ${SRC_DIR}.zip ${SRC_DIR}"
 zip -r ${SRC_DIR}.zip ${SRC_DIR}
 
 # Send zipped src directory & the update script to remote
-#CMD1="scp -P ${HOST_PORT} ${UPDATE_SCRIPT_PATH} ${SRC_DIR}.zip ${USERNAME}@${HOSTNAME}:${API_SERVER_DIR}"
-CMD1="scp ${SRC_DIR}.zip ${UPDATE_SCRIPT_PATH} ${RUN_SCRIPT_PATH} root@raspberrypiteamb.local:${API_SERVER_DIR}/"
+CMD1="scp -P ${PORT} ${SRC_DIR}.zip ${UPDATE_SCRIPT_PATH} ${RUN_SCRIPT_PATH} ${USERNAME}@${HOST}:${API_SERVER_DIR}"
+# CMD1="scp ${SRC_DIR}.zip ${UPDATE_SCRIPT_PATH} ${RUN_SCRIPT_PATH} root@raspberrypiteamb.local:${API_SERVER_DIR}/"
 echo "cmd: $CMD1"
 eval "$CMD1"
 
 # Run the update script on the remote
-#CMD2="ssh -p ${HOST_PORT} ${USERNAME}@${HOSTNAME}:${API_SERVER_DIR}/ 'cd ${API_SERVER_DIR}; ./${UPDATE_SCRIPT_NAME}'"
-CMD2="ssh root@raspberrypiteamb.local 'cd ${API_SERVER_DIR}; ./${UPDATE_SCRIPT_NAME} ${API_SERVER_PORT};'"
+CMD2="ssh -p ${PORT} ${USERNAME}@${HOST} 'cd ${API_SERVER_DIR}; ./${UPDATE_SCRIPT_NAME} ${API_SERVER_PORT};'"
+#CMD2="ssh root@raspberrypiteamb.local 'cd ${API_SERVER_DIR}; ./${UPDATE_SCRIPT_NAME} ${API_SERVER_PORT};'"
 echo "cmd: $CMD2"
 eval "$CMD2"
 
