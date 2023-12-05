0751537ddcf53143d1ff8fbdc33db25d4c62bc6f
sbenetz <shanebenetz@gmail.com>
2023-12-04 16:24:44 -0800

    more node modules

---
+++ microcontroller/build/libraries/BLE/BLEExceptions.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\shane\OneDrive\Documents\UCSD-CSE-118-218-Team-B\microcontroller\build\libraries\BLE\BLEExceptions.cpp.o: \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\libraries\BLE\src\BLEExceptions.cpp
---
+++ microcontroller/build/libraries/BLE/BLESecurity.cpp.d
@@ -0,0 +1,8 @@
+C:\Users\shane\OneDrive\Documents\UCSD-CSE-118-218-Team-B\microcontroller\build\libraries\BLE\BLESecurity.cpp.o: \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\libraries\BLE\src\BLESecurity.cpp \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\libraries\BLE\src\BLESecurity.h \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ C:\Users\shane\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_bt_defs.h
