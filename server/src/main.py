f58e8fc67e60c794d6da41c88bd6de43c581fb17
Jackson-Wheeler <jackson.n.wheeler@gmail.com>
2023-12-07 17:40:48 -0800

    updated server logging

--- server/src/main.py
+++ server/src/main.py
@@ -39,10 +39,9 @@ async def user_login(data: Credentials) -> UserId:
 async def user_get_plants(userId) -> PlantsReturn:
   print(f"--\nGet User's Plants")
   (plants, errorMessage) = database.get_plants(userId)
-  return {
-    "plants": plants,
-    "errorMessage": errorMessage
-  }
+  plantsReturn = PlantsReturn(plants=plants, errorMessage=errorMessage)
+  print(f"Return: {plantsReturn}")
+  return plantsReturn
 
 @app.get("/plants/{plantId}")
 async def get_plant(plantId) -> PlantInfoReturn:
@@ -50,6 +49,7 @@ async def get_plant(plantId) -> PlantInfoReturn:
   (sensorDataLogs, errorMessage) = database.get_plant_sensor_data_logs(plantId)
   (waterHistory, errorMessage) = database.get_plant_water_history(plantId)
   plantInfo = PlantInfoReturn(sensorDataLogs=sensorDataLogs, waterHistory=waterHistory, errorMessage=errorMessage)
+  print(f"Return: {plantInfo}")
   return plantInfo
   
 
@@ -58,6 +58,7 @@ async def get_plant(plantId) -> PlantInfoReturn:
 async def device_initialization(data: DeviceInit) -> str:
   print(f"--\nDevice Initialization: ({data})")
   deviceId = database.device_init(data)
+  print(f"Return: {deviceId}")
   if deviceId is None:
     return ""
   else:
@@ -70,11 +71,6 @@ async def device_checkin(data: DeviceCheckIn) -> DeviceCheckInReturn:
   goalMoisture = database.water_logic(data) # Logic for if to water or not
   print(f"Return: (goalMoisture: {goalMoisture}, sleepTime: {DEVICE_SLEEP_TIME_S})")
   return DeviceCheckInReturn(goalMoisture=goalMoisture, sleepTime=DEVICE_SLEEP_TIME_S)
-
-@app.post("/device/water-confirmation")
-async def device_water_confirm(data: DeviceCredentials) -> str:
-  database.device_water_confirm(data)
-  return "ok"
 
 
 # -- CONTROL COMMANDS --
